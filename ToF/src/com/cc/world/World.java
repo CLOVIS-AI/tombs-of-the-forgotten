a3a67d2a6cce490ba69b7bb9b45443b223dd693c
Ivan Canet <ivan.canet@gmail.com>
2018-03-27 09:44:40 +0200

    Added utility methods for the Locations & Rooms

--- ToF/src/com/cc/world/World.java
+++ ToF/src/com/cc/world/World.java
@@ -7,8 +7,12 @@ package com.cc.world;
 
 import com.cc.players.Entity;
 import com.cc.players.Player;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * The world in which the game is taking place.
@@ -16,7 +20,7 @@ import java.util.Map;
  */
 public class World {
     
-    private Map<Location, Room> rooms;
+    private TreeMap<Location, Room> rooms;
     
     private Player player;
     
@@ -24,4 +28,53 @@ public class World {
     
     private GameState gameState;
     
+    /**
+     * Get every room of the World.
+     * @return A Collection of rooms.
+     */
+    public Collection<Room> getRooms(){
+        return rooms.values();
+    }
+    
+    /**
+     * Get the rooms which location validate a predicate.
+     * @param p a predicate on the location of each room
+     * @return The rooms.
+     */
+    public List<Room> selectRoomsByLocation(Predicate<Location> p){
+        return rooms.keySet()
+                .stream()
+                .filter(p)
+                .map(rooms::get)
+                .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get the rooms that validate a predicate.
+     * @param p a predicate on the room
+     * @return The rooms.
+     */
+    public List<Room> selectRooms(Predicate<Room> p){
+        return rooms.values()
+                .stream()
+                .filter(p)
+                .collect(Collectors.toList());
+    }
+    
+    /**
+     * Get the map of a specific floor.
+     * @param floor The floor you want [0..]
+     * @return A Map of every room and its location.
+     */
+    public Map<Location, Room> getFloor(int floor){
+        if(floor < 0) throw new IllegalArgumentException("Negative values are "
+                + "forbidden in this method, you provided: " + floor);
+        
+        return rooms.entrySet()
+                .stream()
+                .filter(e -> e.getKey().getZ() == floor)
+                .collect(Collectors.toMap(
+                        e -> e.getKey(), 
+                        e -> e.getValue()));
+    }
 }
