03d005e8f09019efbbe1ce1d710eebbd7fd1c4c7
Ivan Canet <ivan.canet@gmail.com>
2018-05-06 13:36:39 +0200

    First implementation of the Dijsktra algorithm in Path

---
+++ ToF/src/com/cc/world/Path.java
@@ -0,0 +1,84 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Canet Ivan & Chourouq Sarah.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cc.world;
+
+import com.cc.players.Entity;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * This class represents a path from a Room to an other. It is represented as a
+ * Queue of Rooms: you can get the next Room where you'd like to go, etc.
+ * @author Ivan Canet
+ */
+public class Path {
+    
+    private final Stack<Room> rooms;
+    
+    public Path(Stack<Room> stack){
+        rooms = stack;
+        
+    }
+    
+    public static Path createPath(World world, Room departure, Room arrival, Entity entity){
+        // Initilization of costs (any=Infinity, first=0)
+        Map<Room, Integer> costs = new HashMap<>();
+        world.getRooms().forEach(r -> costs.put(r, Integer.MAX_VALUE));
+        costs.put(departure, 0);
+        
+        // Initilization of predecessors (any=itself)
+        Map<Room, Room> predecessors = new HashMap<>();
+        world.getRooms().forEach(r -> predecessors.put(r, r));
+        
+        // Search for shortcuts
+        List<Room> unterminated = new ArrayList<>(world.getRooms());
+        for(int i = 0; i < unterminated.size(); i++){
+            Room min = unterminated.stream()
+                    .min((r1, r2) -> costs.get(r1) - costs.get(r2))
+                    .orElseThrow(UnknownError::new);
+            
+            min.getReachableNeighbors(entity)
+                    .filter(r -> unterminated.contains(r))
+                    .filter(r -> costs.get(min) + 1 < costs.get(r))
+                    .forEach(r -> {
+                        costs.put(r, costs.get(min) + 1);
+                        predecessors.put(r, min);
+                    });
+            
+            unterminated.remove(min);
+        }
+        
+        Stack path = new Stack();
+        do{
+            path.add(arrival);
+            arrival = predecessors.get(arrival);
+        }while(arrival != null);
+        
+        return new Path(path);
+    }
+    
+}
