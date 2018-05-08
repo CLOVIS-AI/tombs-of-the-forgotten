ebafe79f96d04db0bdfa9b79e0820bdfa360fbf6
Ivan Canet <ivan.canet@gmail.com>
2018-05-08 17:43:16 +0200

    Tested Entity#nextTick

--- ToF/test/com/cc/players/EntityTest.java
+++ ToF/test/com/cc/players/EntityTest.java
@@ -99,4 +99,39 @@ public class EntityTest {
         assertFalse(p.hasItem(i));
     }
     
+    @Test
+    public void testNextTick() {
+        System.out.println("Entity#nextTick");
+        Entity e1 = new Entity("1", 1, 1, 1, 1) {};
+        Entity e2 = new Entity("2", 2, 2, 2, 2) {};
+        assertFalse(e1.equals(e2)); // check that the test won't break
+        
+        Player p = new Player("p", 3, 3, 3, 3);
+        Room r1 = new Room("r1").setLocation(new Location(0, 0, 0));
+        Room r2 = new Room("r2").setLocation(new Location(-1, 0, 0));
+        Link link = new Opening(r1, r2);
+        link.autoLink();
+        TreeMap<Location, Room> map = World.createTreeMap(asList(r1, r2));
+        World w = new World(map, p, asList(e1, e2));
+        
+        assertTrue(e2.canMoveTo(NORTH));
+        e2.moveTo(NORTH);
+        w.nextTick();
+        
+        assertEquals(new Location(), p.getLocation());
+        assertEquals(new Location(), e1.getLocation());
+        assertEquals(new Location(-1, 0, 0), e2.getLocation());
+        
+        assertTrue(w.getEntities(true).anyMatch(e -> e.equals(e1)));
+        assertTrue(w.getEntities(true).anyMatch(e -> e.equals(e2)));
+        
+        assertTrue("Expected e1, found none", p.getOpponent().isPresent());
+        assertTrue("Expected p, found none", e1.getOpponent().isPresent());
+        System.out.println(e2.getOpponent());
+        assertFalse("Expected none, found " + e2.getOpponent(), e2.getOpponent().isPresent());
+        
+        assertEquals(e1, p.getOpponent().get());
+        assertEquals(p, e1.getOpponent().get());
+    }
+    
 }
