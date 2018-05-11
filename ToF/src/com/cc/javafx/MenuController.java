d5edd0a6cc4c007c5cbf9e9f7ce64c5aff9ac4c1
Chourouq Sarah <sarah.chourouq@etu.u-bordeaux.fr>
2018-05-11 19:05:05 +0200

    copy of javafx work

---
+++ ToF/src/com/cc/javafx/MenuController.java
@@ -0,0 +1,76 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Canet Ivan & Chourouq Sarah.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cc.javafx;
+
+import java.io.File;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+
+/**
+ * FXML Controller class
+ *
+ * @author schourouq
+ */
+public class MenuController implements Initializable {
+
+    @FXML
+    private AnchorPane NewArrow;
+    @FXML
+    private AnchorPane LoadArrow;
+    @FXML
+    private AnchorPane QuitArrow;
+    @FXML
+    private Label NewLabel;
+    @FXML
+    private Label LoadLabel;
+    @FXML
+    private Label QuitLabel;
+    @FXML
+    private Label Title;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Title.setFont(Font.loadFont("Skyrim.ttf", 30));
+        NewArrow.setVisible(false);
+        LoadArrow.setVisible(false);
+        QuitArrow.setVisible(false);
+        NewLabel.setOnMouseEntered(e -> NewArrow.setVisible(true));
+        NewLabel.setOnMouseExited(e -> NewArrow.setVisible(false));
+        LoadLabel.setOnMouseEntered(e -> LoadArrow.setVisible(true));
+        LoadLabel.setOnMouseExited(e -> LoadArrow.setVisible(false));
+        QuitLabel.setOnMouseEntered(e -> QuitArrow.setVisible(true));
+        QuitLabel.setOnMouseExited(e -> QuitArrow.setVisible(false));
+        QuitLabel.setOnMousePressed(e -> Platform.exit());
+        
+    }    
+    
+}
