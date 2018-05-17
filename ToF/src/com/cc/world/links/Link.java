3665b1c65d83a0dd7d42747a51bc13183ce6f1d9
Ivan Canet <ivan.canet@gmail.com>
2018-05-17 11:41:18 +0200

    Added more constructors

--- ToF/src/com/cc/world/links/Link.java
+++ ToF/src/com/cc/world/links/Link.java
@@ -93,6 +93,23 @@ public abstract class Link implements Save<JsonObject> {
     }
     
     /**
+     * Creates a link between two rooms, that is open by default.
+     * @param rooms the two rooms (in no particular order)
+     */
+    public Link(Room... rooms){
+        this(rooms[0], rooms[1]);
+    }
+    
+    /**
+     * Creates a link between two rooms, that is open by default.
+     * @param r1 the first room
+     * @param r2 the second room
+     */
+    public Link(Room r1, Room r2){
+        this(r1, r2, true);
+    }
+    
+    /**
      * Returns the two rooms as an array.
      * @return An array of the two rooms linked by this object.
      */
