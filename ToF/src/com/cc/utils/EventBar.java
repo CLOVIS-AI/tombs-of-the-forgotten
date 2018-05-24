abed73170ee32203055861add7b434f22abf069a
Ivan Canet <ivan.canet@gmail.com>
2018-05-24 17:01:15 +0200

    Enabled method-chaining in EventBar

--- ToF/src/com/cc/utils/EventBar.java
+++ ToF/src/com/cc/utils/EventBar.java
@@ -66,25 +66,31 @@ public class EventBar extends Bar {
     /**
      * Sets the action to be taken on the event of the bar being full.
      * @param onFull the action (the parameter is this bar)
+     * @return This object, to allow method-chaining.
      */
-    public void setOnFull(Consumer<EventBar> onFull) {
+    public EventBar setOnFull(Consumer<EventBar> onFull) {
         this.onFull = onFull;
+        return this;
     }
 
     /**
      * Sets the action to be taken on the event of the bar being empty.
      * @param onEmpty the action (the parameter is this bar)
+     * @return This object, to allow method-chaining.
      */
-    public void setOnEmpty(Consumer<EventBar> onEmpty) {
+    public EventBar setOnEmpty(Consumer<EventBar> onEmpty) {
         this.onEmpty = onEmpty;
+        return this;
     }
     
     /**
      * Sets the action to be taken on the event of the bar updated.
      * @param onUpdate the action (the parameter is this bar)
+     * @return This object, to allow method-chaining.
      */
-    public void setOnBonusUpdate(Consumer<EventBar> onUpdate) {
+    public EventBar setOnBonusUpdate(Consumer<EventBar> onUpdate) {
         this.onBonusUpdate = onUpdate;
+        return this;
     }
     
     @Override
